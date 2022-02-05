a92f216c48867d35476bf4efacf7a5d8a3b1f684
NikolaevAnton <mjop41@gmail.com>
2022-02-05 16:55:41 +0300

    Initial Commit

---
+++ PersonsBoard/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PersonsBoard
+//
+//  Created by Anton Nikolaev on 05.02.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ PersonsBoard/PersonsBoardApp.swift
@@ -0,0 +1,17 @@
+//
+//  PersonsBoardApp.swift
+//  PersonsBoard
+//
+//  Created by Anton Nikolaev on 05.02.2022.
+//
+
+import SwiftUI
+
+@main
+struct PersonsBoardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
